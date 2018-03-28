bd68ada39a04669487b8b99588408a54b7d61043
lifeng <lifeng@qq.com>
2018-03-29 02:41:47 +0800

    预报名信息表

---
+++ src/main/java/com/nbs/jiaxiao/domain/po/SignStudent.java
@@ -0,0 +1,84 @@
+package com.nbs.jiaxiao.domain.po;
+
+import com.nbs.jiaxiao.domain.base.Base;
+
+/**
+ * 
+ * 预报名学员表
+ *
+ */
+public class SignStudent extends Base{
+	
+	/*  */
+	private java.lang.Integer id;
+	/* 用户姓名 */
+	private java.lang.String userName;
+	/* 微信号openId */
+	private java.lang.String openId;
+	/* 手机号 */
+	private java.lang.String mobile;
+	/* 备注 */
+	private java.lang.String remark;
+	/* 状态, 0:未查看， 1：已查看 */
+	private java.lang.String status;
+	
+	
+	
+	public java.lang.Integer getId() {
+		return id;
+	}
+
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+	
+	public java.lang.String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(java.lang.String userName) {
+		if (userName != null) {
+			this.userName = userName.trim();
+		}
+	}
+	
+	public java.lang.String getOpenId() {
+		return openId;
+	}
+
+	public void setOpenId(java.lang.String openId) {
+		if (openId != null) {
+			this.openId = openId.trim();
+		}
+	}
+	
+	public java.lang.String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(java.lang.String mobile) {
+		if (mobile != null) {
+			this.mobile = mobile.trim();
+		}
+	}
+	
+	public java.lang.String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(java.lang.String remark) {
+		if (remark != null) {
+			this.remark = remark.trim();
+		}
+	}
+	
+	public java.lang.String getStatus() {
+		return status;
+	}
+
+	public void setStatus(java.lang.String status) {
+		if (status != null) {
+			this.status = status.trim();
+		}
+	}
+}
---
+++ src/main/java/com/nbs/jiaxiao/service/db/impl/SignStudentServiceImpl.java
@@ -0,0 +1,120 @@
+package com.nbs.jiaxiao.service.db.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.nbs.jiaxiao.domain.po.SignStudent;
+import com.nbs.jiaxiao.exception.ConcurrentException;
+import com.nbs.jiaxiao.mapper.SignStudentMapper;
+
+@Service
+public class SignStudentServiceImpl {
+	
+	@Resource
+	private SignStudentMapper signStudentMapper;
+	
+	/**
+	 * 保存
+	 * @param signStudent
+	 * @return
+	 */
+	public int insert(SignStudent signStudent){
+		return signStudentMapper.insert(signStudent);
+	}
+	
+	/**
+	 * 根据主键更新
+	 * @param signStudent
+	 * @return
+	 */
+	public void updateByPriKey(SignStudent signStudent){
+		if(signStudentMapper.updateByPriKey(signStudent) != 1) {
+			throw new ConcurrentException("并发更新SignStudent失败");
+		}
+	}
+	
+	/**
+	 * 根据条件逻辑删除 慎用
+	 * @param con 条件
+	 * @return
+	 */
+	public int delete(SignStudent con){
+		return signStudentMapper.delete(con);
+	}
+	
+	/**
+	 * 根据主键逻辑删除
+	 * @param id 主键
+	 * @return
+	 */
+	public int deleteByPriKey(Integer id){
+		return signStudentMapper.deleteByPriKey(id);
+	}
+	
+	/**
+	 * 根据条件物理删除 慎用
+	 * @param con
+	 * @return
+	 */
+	public int deleteActual(SignStudent con){
+		return signStudentMapper.deleteActual(con);
+	}
+	
+	/**
+	 * 根据主键物理删除 慎用
+	 * @param id
+	 * @return
+	 */
+	public int deleteActualByPriKey(Integer id){
+		return signStudentMapper.deleteActualByPriKey(id);
+	}
+
+	/**
+	 * 清理>=主键的已删除数据
+	 * @param id 主键
+	 * @return
+	 */
+	public int clean(Integer id){
+		return signStudentMapper.clean(id);
+	}
+
+	/**
+	 * 根据主键查询
+	 * @param id
+	 * @return
+	 */
+	public SignStudent selectByPriKey(Integer id){
+		return signStudentMapper.selectByPriKey(id);
+	}
+	
+	/**
+	 * 根据条件查询
+	 * @param con
+	 * @return
+	 */
+	public List<SignStudent> selectList(SignStudent con){
+		return signStudentMapper.selectList(con);
+	}
+	
+	/**
+	 * 根据条件计数
+	 * @param con
+	 * @return
+	 */
+	public long selectCount(SignStudent con){
+		return signStudentMapper.selectCount(con);
+	}
+	
+	/**
+	 * 根据条件判断是否存在
+	 * @param con 条件
+	 * @return 存在的第一个值的主键
+	 */
+	public boolean exist(SignStudent con){
+		return signStudentMapper.exist(con) != null;
+	}
+
+}
