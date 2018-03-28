cd49c66f4246f4a5e8c21758330cb0d4029ecd77
lifeng <lifeng@qq.com>
2018-03-29 01:57:27 +0800

    修改

--- src/main/java/com/nbs/jiaoxiao/common/DateUtils.java
+++
@@ -1,5 +0,0 @@
-package com.nbs.jiaoxiao.common;
-
-public class DateUtils extends org.apache.commons.lang3.time.DateUtils{
-
-}
--- src/main/java/com/nbs/jiaoxiao/constant/RedisKey.java
+++
@@ -1,6 +0,0 @@
-package com.nbs.jiaoxiao.constant;
-
-public class RedisKey {
-
-	public static final String SESSION_ = "SESSION_";
-}
