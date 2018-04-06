d1bb1c57d7817d24f11d6614905ac555c03a88c7
lifeng <lifeng@qq.com>
2018-04-06 16:59:28 +0800

    销售员预报名信息

---
+++ src/main/java/com/nbs/jiaxiao/mapper/PreSellerMapper.java
@@ -0,0 +1,90 @@
+package com.nbs.jiaxiao.mapper;
+
+import java.util.List;
+
+import com.nbs.jiaxiao.domain.po.PreSeller;
+
+public interface PreSellerMapper {
+	
+	/**
+	 * 保存
+	 * @param preSeller
+	 * @return
+	 */
+	public int insert(PreSeller preSeller);
+	
+	/**
+	 * 根据主键更新
+	 * @param preSeller
+	 * @return
+	 */
+	public int updateByPriKey(PreSeller preSeller);
+	
+	/**
+	 * 根据条件逻辑删除 慎用
+	 * @param con 条件
+	 * @return
+	 */
+	public int delete(PreSeller con);
+	
+	/**
+	 * 根据主键逻辑删除
+	 * @param id 主键
+	 * @return
+	 */
+	public int deleteByPriKey(Integer id);
+	
+	/**
+	 * 根据条件物理删除 慎用
+	 * @param con
+	 * @return
+	 */
+	public int deleteActual(PreSeller con);
+	
+	/**
+	 * 根据主键物理删除 慎用
+	 * @param id
+	 * @return
+	 */
+	public int deleteActualByPriKey(Integer id);
+
+	/**
+	 * 清理>=主键的已删除数据
+	 * @param id 主键
+	 * @return
+	 */
+	public int clean(Integer id);
+
+	/**
+	 * 根据主键查询
+	 * @param id
+	 * @return
+	 */
+	public PreSeller selectByPriKey(Integer id);
+	
+	/**
+	 * 根据条件查询
+	 * @param con
+	 * @return
+	 */
+	public List<PreSeller> selectList(PreSeller con);
+	
+	/**
+	 * 根据条件计数
+	 * @param con
+	 * @return
+	 */
+	public long selectCount(PreSeller con);
+	
+	/**
+	 * 根据条件判断是否存在
+	 * @param con 条件
+	 * @return 存在的第一个值的主键
+	 */
+	public Integer exist(PreSeller con);
+	
+	/* customized code start */
+	
+	/* customized code end */
+
+}
---
+++ src/main/java/com/nbs/jiaxiao/service/db/impl/PreSellerServiceImpl.java
@@ -0,0 +1,125 @@
+package com.nbs.jiaxiao.service.db.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.nbs.jiaxiao.domain.po.PreSeller;
+import com.nbs.jiaxiao.exception.ConcurrentException;
+import com.nbs.jiaxiao.mapper.PreSellerMapper;
+import com.nbs.jiaxiao.service.db.PreSellerService;
+
+@Service
+public class PreSellerServiceImpl implements PreSellerService{
+	
+	@Resource
+	private PreSellerMapper preSellerMapper;
+	
+	/**
+	 * 保存
+	 * @param preSeller
+	 * @return
+	 */
+	public int insert(PreSeller preSeller){
+		return preSellerMapper.insert(preSeller);
+	}
+	
+	/**
+	 * 根据主键更新
+	 * @param preSeller
+	 * @return
+	 */
+	public void updateByPriKey(PreSeller preSeller){
+		if(preSellerMapper.updateByPriKey(preSeller) != 1) {
+			throw new ConcurrentException("并发更新PreSeller失败");
+		}
+	}
+	
+	/**
+	 * 根据条件逻辑删除 慎用
+	 * @param con 条件
+	 * @return
+	 */
+	public int delete(PreSeller con){
+		return preSellerMapper.delete(con);
+	}
+	
+	/**
+	 * 根据主键逻辑删除
+	 * @param id 主键
+	 * @return
+	 */
+	public int deleteByPriKey(Integer id){
+		return preSellerMapper.deleteByPriKey(id);
+	}
+	
+	/**
+	 * 根据条件物理删除 慎用
+	 * @param con
+	 * @return
+	 */
+	public int deleteActual(PreSeller con){
+		return preSellerMapper.deleteActual(con);
+	}
+	
+	/**
+	 * 根据主键物理删除 慎用
+	 * @param id
+	 * @return
+	 */
+	public int deleteActualByPriKey(Integer id){
+		return preSellerMapper.deleteActualByPriKey(id);
+	}
+
+	/**
+	 * 清理>=主键的已删除数据
+	 * @param id 主键
+	 * @return
+	 */
+	public int clean(Integer id){
+		return preSellerMapper.clean(id);
+	}
+
+	/**
+	 * 根据主键查询
+	 * @param id
+	 * @return
+	 */
+	public PreSeller selectByPriKey(Integer id){
+		return preSellerMapper.selectByPriKey(id);
+	}
+	
+	/**
+	 * 根据条件查询
+	 * @param con
+	 * @return
+	 */
+	public List<PreSeller> selectList(PreSeller con){
+		return preSellerMapper.selectList(con);
+	}
+	
+	/**
+	 * 根据条件计数
+	 * @param con
+	 * @return
+	 */
+	public long selectCount(PreSeller con){
+		return preSellerMapper.selectCount(con);
+	}
+	
+	/**
+	 * 根据条件判断是否存在
+	 * @param con 条件
+	 * @return 存在的第一个值的主键
+	 */
+	public boolean exist(PreSeller con){
+		return preSellerMapper.exist(con) != null;
+	}
+	
+	/* customized code start */
+	
+	/* customized code end */
+
+}
